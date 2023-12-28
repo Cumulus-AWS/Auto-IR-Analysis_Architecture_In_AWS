b553d66de98bf028736e39ea24abfea49626b732
park1NG <113297396+park1NG@users.noreply.github.com>
2023-12-29 00:40:46 +0900

    Create 5-GuardDuty-Forensic-Discord-Alarm.py

---
+++ lambda_function/5-GuardDuty-Forensic-Discord-Alarm.py
@@ -0,0 +1,52 @@
+import requests
+import json
+
+# Lambda function to send a notification to Discord when forensic automation is executed
+
+def lambda_handler(event, context):
+    # Discord Webhook URL for sending messages
+    url = "your-Webhook-URL"
+
+    # Set default avatar and username for the Discord message
+    data = {
+        "avatar_url": "your-avatar-URL",
+        "username": "Cumulus AWS Guard-Duty",
+    }
+    
+    # Extract instance ID and finding ID from the CloudWatch event
+    InstanceID = event["detail"]["resource"]["instanceDetails"]["instanceId"]
+    FindingID = event["detail"]["id"]
+
+    # Create Discord message payload with embedded content
+    data["embeds"] = [
+        {
+            "title": f"**Automation Forensic is executed, in {InstanceID}**",
+            "description": (
+                "\n\n"
+                # Displaying details of the AWS GuardDuty finding
+                "* Type : " + event["detail"]["type"] + "\n\n"
+                "* Title : " + event["detail"]["title"] + "\n\n"
+                "* Severity : " + str(event["detail"]["severity"]) + "\n\n" 
+                "* Description : " + event["detail"]["description"] + "\n\n"
+                # Include a link to the AWS GuardDuty console for more details
+                f"* Link : [AWS GuardDuty](https://ap-northeast-2.console.aws.amazon.com/guardduty/home?region=ap-northeast-2#/findings?macros=current&fId={FindingID})"
+            )
+        }
+    ]
+
+    # Send the Discord message using the Webhook URL
+    result = requests.post(url, json=data)
+    
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        # Handle HTTP error if the message delivery fails
+        return {
+            'statusCode': 400,
+        }
+    else:
+        # Return a success response if the message is delivered successfully
+        return {
+            'statusCode': 200,
+            'body': json.dumps("Payload delivered successfully, code {}.".format(result.status_code))
+        }
